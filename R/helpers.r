bcd96fc3638eecc7bf42c75deaf9e6f826d69936
cmjt <cmjt@st-andrews.ac.uk>
2018-11-16 15:54:47 +1300

    sorting out get.params

--- R/helpers.r
+++ R/helpers.r
@@ -161,7 +161,7 @@ setGeneric("ll",
 setMethod("ll",
           c(fit = "mmre"),
           function(fit){
-              pars <- get.params(fit, TRUE)[,1]
+              pars <- get.params(fit, FALSE)[,1]
               nll <- fit@fit$fn(pars)
               -nll
           }
