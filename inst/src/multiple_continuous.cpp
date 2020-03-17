73a3f6f74c6e4a98db3592f03819bc737613f61f
cmjt <cmjonestodd@gmail.com>
2020-03-17 15:50:11 +1300

    trying to generalize

--- inst/src/multiple_continuous.cpp
+++ inst/src/multiple_continuous.cpp
@@ -3,7 +3,7 @@
 #include <Eigen/Dense>
 
 using namespace Eigen;
-// Templates for fitting a two state Markov model to multiple individuals with transition matrix Q
+// Templates for fitting an N state Markov model to multiple individuals with transition matrix Q
 // NO random effects
 
 // state_list and time_list are both templates that read a list in from R of
@@ -37,19 +37,23 @@ Type objective_function<Type>::operator() (){
   DATA_FACTOR(ID);
   DATA_STRUCT(states, state_list); //an array of numeric states (i.e., in whale example 1s and 2s) for each whale
   DATA_STRUCT(times, time_list); //an array of times for each whale
-  PARAMETER_VECTOR(log_baseline);//a vector of the log off diagonal transition intensities to be estimated using ML
-  vector<Type> q = exp(log_baseline); // declare q
+  PARAMETER_MATRIX(Q); // Q matrix (diagonal entries will be ignored)
+  int nstates = Q.row(0).size(); // number of states
+  for (int k = 0; k < nstates; k++){
+    Q(k,k) = 0;
+    Type sum = Q.row(k).sum();
+    Q(k,k) = -sum;
+  }
+  std::cout << Q << "\n";
   int wh =  NLEVELS(ID); // number of whales
   vector<Type> ll(wh);
-  matrix<Type> Q(2,2);
-  Q(0,0) = -q(0); Q(0,1) = q(0); Q(1,0) = q(1); Q(1,1) = -q(1); 
   //contribution from observed data
   for (int j = 0; j < wh; j++){
     vector<Type> tem = times(j);
     vector<Type> sem = states(j); 
     int t = tem.size();
-      for (int i = 0; i < (t-1); i++){
-      	Type temp = tem(i+1) - tem(i);
+    for (int i = 0; i < (t-1); i++){
+      Type temp = tem(i+1) - tem(i);
       	matrix<Type> Qt = Q*temp;
       	matrix<Type> P = atomic::expm(Qt);
 	int x = CppAD::Integer(sem(i));
